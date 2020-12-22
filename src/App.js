b3328a665b0ba6f0d1916418340b42e5c168bafc
dimarrri <dimarrri@outlook.com>
2020-12-22 15:31:41 +0300

    last commit

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1 style={{color: 'red' }}>Heloo world</h1> 
+      <h1 style={{color: 'green' }}>Heloo world</h1> 
     </div>
   );
 }
